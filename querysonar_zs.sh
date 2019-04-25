08467558735372b9ba87d5d425ad9730fe6730d8
HuangZhengSi <huangzhengsi@tron.network>
2019-04-25 18:19:05 +0800

    update

--- querysonar_zs.sh
+++ querysonar_zs.sh
@@ -1,6 +1,6 @@
 
 PassFlag=`curl -s 'https://sonarcloud.io/api/project_badges/measure?project=java-tron_zs&metric=alert_status'|grep -A4 "quality gate"|grep "pass"|wc -l`
-
+echo "Please visit https://sonarcloud.io/dashboard?id=java-tron_zs for more details"
 if [ $PassFlag -eq 0 ]; then
     echo "Sonar Check Failed"
     exit 1
