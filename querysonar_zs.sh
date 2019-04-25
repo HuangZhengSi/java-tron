55192bc9e600c34ee511934bea5f6968f8af6777
HuangZhengSi <huangzhengsi@tron.network>
2019-04-25 17:57:04 +0800

    modify circleci

---
+++ querysonar_zs.sh
@@ -0,0 +1,11 @@
+
+PassFlag=`curl -s 'https://sonarcloud.io/api/project_badges/measure?project=java-tron_zs&metric=alert_status'|grep -A4 "quality gate"|grep "pass"|wc -l`
+
+if [ $PassFlag -eq 0 ]; then
+    echo "Sonar Check Failed"
+    exit 1
+else
+    echo "Sonar Check Pass"
+    exit 0
+fi
+
